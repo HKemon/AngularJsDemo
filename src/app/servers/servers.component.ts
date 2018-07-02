0cb5991afb7b78b80acbc8feedfba743103b9baa
emon <hk.emon.007@gmail.com>
2018-07-02 16:34:22 +0600

    init

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server is created';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  serverCreateionMethod() {
+    this.serverCreationStatus = 'server is created';
+  }
+}
